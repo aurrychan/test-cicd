4ec9b5b9e779093233c39dc946bf536b5919fbd8
Sree Harsha Angara <sreeharsha.angara@cypress.com>
2021-02-14 08:35:59 -0800

    Added test case

--- test_palin.py
+++ test_palin.py
@@ -7,6 +7,7 @@ class TestTest(unittest.TestCase):
             self.assertEqual(palin_int.Solution.is_palindrome(self, 121), True)
             self.assertEqual(palin_int.Solution.is_palindrome(self, -121), False)
             self.assertEqual(palin_int.Solution.is_palindrome(self, 1231), False)
+            self.assertEqual(palin_int.Solution.is_palindrome(self, 11231), False)
             self.assertEqual(palin_int.Solution.is_palindrome(self, 0), True)
 
 if __name__ == '__main__':
